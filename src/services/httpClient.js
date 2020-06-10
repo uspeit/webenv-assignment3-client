bffe726b4f4f25b613362af9537343243b4ac53e
Eitan Fedenko <eitan.lp@gmail.com>
2020-06-11 02:11:50 +0300

    Fixed comments in httpClient

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
 
+// Define our token source for lazy loading later
 const getToken = () => localStorage.getItem('token');
 
 // Create client
 export default axios.create({
     baseURL: `https://soupify.herokuapp.com/api`,
     withCredentials: false,
-    // Using lambda function to create headers on each request
+    // Try attaching token upon sending a request
     transformRequest: [function (data, headers) {
         const token = getToken();
         if (token)
             headers['Authorization'] = `Bearer ${token}`;
         return JSON.stringify(data)
     }],
+    // Headers
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
